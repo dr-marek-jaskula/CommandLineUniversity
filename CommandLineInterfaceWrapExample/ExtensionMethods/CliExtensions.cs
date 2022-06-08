719cce2ff994a3a17b66602301907ebcc9b30218
Marek <Marek@DESKTOP-8BJGCQ1>
2022-06-08 23:37:06 +0200

    Add CLI.Wrap and some cleaning

---
+++ CommandLineInterfaceWrapExample/ExtensionMethods/CliExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CliWrap.Builders;
+
+namespace CommandLineInterfaceWrapExample.ExtensionMethods;
+
+public static class CliExtensions
+{
+    public static ArgumentsBuilder AddOption(
+        this ArgumentsBuilder args,
+        string name,
+        string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return args;
+
+        return args.Add(name).Add(value);
+    }
+}
